6ff67dd40e09a67e4997e87a17422046f98f9200
Duane Bekaert <duane.bekaert@gmail.com>
2016-08-23 23:40:41 +0200

    Created the basic public interface

--- main.go
+++ main.go
@@ -8,9 +8,21 @@ import (
 var outputDir string
 var port string
 
+var sendMail bool
+
+var emailAddress string
+var smtp string
+var smtpName string
+var smtpPwd string
+
 func init() {
-	flag.StringVar(&outputDir, "o", "~/gvp", "output directory")
+	flag.StringVar(&outputDir, "o", "~/gvp_tray", "output directory")
 	flag.StringVar(&port, "p", ":9100", "port gvp listens on")
+	flag.BoolVar(&sendMail, "m", false, "send prints via email")
+	flag.StringVar(&port, "a", ":9100", "email address")
+	flag.StringVar(&smtp, "s", ":9100", "smtp server and port")
+	flag.StringVar(&smtpName, "su", ":9100", "smtp user name")
+	flag.StringVar(&smtpPwd, "sp", ":9100", "smtp user password")
 }
 
 func main() {
